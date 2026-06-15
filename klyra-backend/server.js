1bdc37cdaa5a8cb4ec83c84538458b3a6898fc68
Krutikakhamkar-7 <krutikakhamkar07@gmail.com>
2026-06-15 13:57:23 +0530

    Mongo DB Connected

--- klyra-backend/server.js
+++ klyra-backend/server.js
@@ -1,3 +1,8 @@
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
 const express = require("express");
 
 const app = express();
@@ -6,7 +11,7 @@ app.get("/", (req, res) => {
   res.send("Klyra Backend Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
