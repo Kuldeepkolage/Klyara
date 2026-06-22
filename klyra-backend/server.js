43d75db84b2baae90280022cf969d7644c16ca63
Krutikakhamkar-7 <krutikakhamkar07@gmail.com>
2026-06-22 14:52:32 +0530

    Backend APIs and MongoDB setup

--- klyra-backend/server.js
+++ klyra-backend/server.js
@@ -2,14 +2,11 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
 dotenv.config();
+
 connectDB();
+
 const express = require("express");
-
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Klyra Backend Running");
-});
 
 const PORT = process.env.PORT || 5000;
 
